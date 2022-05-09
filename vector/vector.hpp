de47a31751aa32e199b29c314aeb12552a40a9b4
mokellat <kellatis55@gmail.com>
2022-05-09 20:54:08 +0100

    iterator typedef

--- vector/vector.hpp
+++ vector/vector.hpp
@@ -3,9 +3,19 @@
 
 #include <iostrem>
 
-template < class T, class Alloc = allocator<T>>
+template < class T, class Alloc = std::allocator<T>>
 class vector
 {
+    public:
+        typedef T value_type;
+        typedef Alloc allocator_type;
+        typedef typename allocator_type::reference reference;
+        typedef typename allocator_type::const_reference const_reference;
+        typedef typename allocator_type::pointer pointer;
+        typedef typename allocator_type::const_pointer const_pointer;
 
 }
 #endif
+
+
+//what does typename there
