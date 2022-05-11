b32450e2b4abda9591e11b1e30519d1dd11f4237
mokellat <kellatis55@gmail.com>
2022-05-11 17:16:41 +0100

    started implementing operators

--- vector/iterator.hpp
+++ vector/iterator.hpp
@@ -1,11 +1,48 @@
-#ifndef IRERATOR_HPP
-#define IRERATOR_HPP
+#ifndef ITERATOR_HPP
+#define ITERATOR_HPP
 
 #include "library.hpp"
 
 template <class T>
-class Iterator
+class MyIterator
 {
+    public:
+        //typedefs
+        typedef T   value_type;
+        typedef T*  pointer;
+        typedef T&  reference;
+    
+    private:
+        pointer _ptr;
 
+    public:
+        // default constructor
+        MyIterator(pointer cp)
+        {
+            _ptr = abc;
+        }
+        // copy constructor
+        MyIterator(MyIterator &cp)
+        {
+            this->_ptr = cp._ptr;
+        }
+        // assignment overload
+        MyIterator &operator=(MyIterator &rhs)
+        {
+            this->_ptr = rhs._ptr;
+            return (*this); //forget what this do
+        }
+        // destructor
+        ~MyIterator()
+        {
+            std::cout << "Desctructor Called" << std::endl;
+        }
+
+        // operators begin
+        bool &operator==(MyIterator &rhs)
+        {
+            this->_ptr = rhs._ptr;
+            return (*this); //forget what this do
+        }
 };
 #endif
--- vector/vector.hpp
+++ vector/vector.hpp
@@ -7,18 +7,18 @@ template < class T, class Alloc = std::allocator<T>>
 class vector
 {
     public:
-        typedef T value_type;
-        typedef Alloc allocator_type;
-        typedef ptrdiff_t difference_type;
-        typedef size_t size_type;
-        typedef typename allocator_type::reference reference;
-        typedef typename allocator_type::const_reference const_reference;
-        typedef typename allocator_type::pointer pointer;
-        typedef typename allocator_type::const_pointer const_pointer;
+        typedef T                                           value_type;
+        typedef Alloc                                       allocator_type;
+        typedef ptrdiff_t                                   difference_type;
+        typedef size_t                                      size_type;
+        typedef typename allocator_type::reference          reference;
+        typedef typename allocator_type::const_reference    const_reference;
+        typedef typename allocator_type::pointer            pointer;
+        typedef typename allocator_type::const_pointer      const_pointer;
 
         // iterators
-        typedef Iterator<value_type> iterator;
-        typedef Iterator<const value_type> const_iterator;
+        typedef MyIterator<value_type>        iterator;
+        typedef MyIterator<const value_type>  const_iterator;
 
         //reverse iterator
 };
