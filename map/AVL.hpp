db199d8654157571122ccd4c0dae53f04dc4836e
mokellat <kellatis55@gmail.com>
2022-09-21 09:13:22 +0100

    pre incre and decre left

--- map/AVL.hpp
+++ map/AVL.hpp
@@ -6,6 +6,7 @@ template<class T>
 class Node 
 {
 	public:
+		T		value;
 		int		key;
 		Node	*left;
 		Node	*right;
--- map/map_iterator.hpp
+++ map/map_iterator.hpp
@@ -10,6 +10,74 @@ namespace ft
     class mapIterator
     {
         public:
-            
+            //typedefs
+            typedef T                   value_type;
+            typedef Node<value_type>    node_type;
+            typedef typename std::bidirectional_iterator_tag	iterator_category;
+
+        private:
+            Node<value_type> *_node;
+
+        public:
+            mapIterator() : _node(){}
+
+            mapIterator(node_type *node) : _node(node){}
+
+            mapIterator(mapIterator &it)
+            {
+                _node = it._node;
+            }
+
+            ~mapIterator(){}        
+
+            mapIterator &operator=(mapIterator &rhs)
+            {
+                _node = rhs._node;
+                return *this;
+            }
+
+            bool &operator==(mapIterator &rhs)
+            {
+                return (_node == rhs.node);
+            }
+
+            bool &operator!=(mapIterator &rhs)
+            {
+                return (node != rhs._node);
+            }
+
+            reference	operator*() 
+            {
+                return *(_node->value); 
+            }
+
+		    pointer	operator->()
+            {
+                return _node->value; 
+            }
+
+            mapIterator &operator++()
+            {
+                // for later
+            }
+
+            mapIterator &operator++(int)
+            {
+                mapIterator it(*this);
+                operator++();
+                return it;
+            }
+
+             mapIterator &operator--()
+            {
+                // for later
+            }
+
+            mapIterator &operator--(int)
+            {
+                mapIterator it(*this);
+                operator--();
+                return it;
+            }
     };
 }
